b4f0e3b8ef2292fb025d7a0baabef1e78e137eab
Ваня Кориков <profftols@yandex.ru>
2022-06-29 23:24:36 +0500

    11

---
+++ Lessions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lessions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World1!");
+        }
+    }
+}
